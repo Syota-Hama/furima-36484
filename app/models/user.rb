3ade46b91f46bdf4acac6185425f795e969993d0
Syota-Hama <77922154+Syota-Hama@users.noreply.github.com>
2021-12-26 22:06:23 +0900

    エラーメッセージの重複の解消

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+
+  validates :password, presence: true, allow_blank: true
+  validates :password, format: {with: VALID_PASSWORD_REGEX, message: "Password is invalid. Include both letters and numbers"}
+
   with_options presence: true do
     validates :nickname
-    validates :password, format: {with: VALID_PASSWORD_REGEX, message: "Password is invalid. Include both letters and numbers"}
     validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "First name is invalid. Input full-width characters"}
     validates :family_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "Last name is invalid. Input full-width characters"}
     validates :first_name_hurigana, format: {with: /\A[ァ-ヶー]+\z/, message: "First name kana is invalid. Input full-width katakana characters"}
