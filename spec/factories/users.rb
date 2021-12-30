ddb40282dba1cae62e92c1a0c2f0ffe302debfbb
Syota-Hama <77922154+Syota-Hama@users.noreply.github.com>
2021-12-31 00:42:36 +0900

    ファクトリーボットの変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     transient do
       person { Gimei.name }
     end
-    nickname              { Faker::Name.initials(number: 4) }
+    nickname              { Faker::Name.name }
     email                 { Faker::Internet.free_email }
     password              { Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
