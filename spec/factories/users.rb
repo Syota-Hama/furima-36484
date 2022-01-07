af525048d8f50e583e5972f5e9adbdffc1b71a57
Syota-Hama <77922154+Syota-Hama@users.noreply.github.com>
2022-01-07 14:20:18 +0900

    factorybotの追記

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     end
     nickname              { Faker::Name.name }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Internet.password(min_length: 6) }
+    password              { '1a' + Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
     first_name            { person.first.kanji }
     family_name           { person.last.kanji }
