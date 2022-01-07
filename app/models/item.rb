9827831ee4e7d43032612e727bbaa1ff9feca20c
Syota-Hama <77922154+Syota-Hama@users.noreply.github.com>
2022-01-07 19:44:14 +0900

    activehash payment prefecture　の表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,13 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :Category
+  belongs_to :category
+  belongs_to :stasus
+  belongs_to :peyment
+  belongs_to :Prefecture
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank" }
-
   validates :status_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :payment_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank" }
+  
 end
