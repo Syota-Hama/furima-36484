dfb9abe0e3cc30c50310f3de5ca112fd29d8b4b7
Syota-Hama <77922154+Syota-Hama@users.noreply.github.com>
2022-01-07 19:49:03 +0900

    activehash delivery　の表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,10 +4,12 @@ class Item < ApplicationRecord
   belongs_to :stasus
   belongs_to :peyment
   belongs_to :Prefecture
+  belongs_to :delivery
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank" }
   validates :status_id, numericality: { other_than: 1 , message: "can't be blank" }
   validates :payment_id, numericality: { other_than: 1 , message: "can't be blank" }
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank" }
-  
+  validates :delivery_id, numericality: { other_than: 1 , message: "can't be blank" }
+
 end
