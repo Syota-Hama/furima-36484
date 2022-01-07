145107d3c0e020c3a754edddbe58bfe32e127dc1
Syota-Hama <77922154+Syota-Hama@users.noreply.github.com>
2022-01-07 17:43:57 +0900

    activehash category のバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :Category
+
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank" }
 end
