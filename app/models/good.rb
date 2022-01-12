f0127c30ef8a0059b891c2dc0bdb28248b1bacab
Syota-Hama <77922154+Syota-Hama@users.noreply.github.com>
2022-01-12 17:51:55 +0900

    factorybotの設定、itの記述

--- app/models/good.rb
+++ app/models/good.rb
@@ -13,6 +13,11 @@ class Good < ApplicationRecord
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :delivery_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :price, numericality: { only_integer: true }
+  validates :item_image, presence: true, unless: :was_attached?
 
   validates :price, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
+
+  def was_attached?
+    self.item_image.attached?
+  end
 end
--- spec/models/good_spec.rb
+++ spec/models/good_spec.rb
@@ -1,5 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe Good, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @goods = FactoryBot.build(:good)
+    @goods.item_image = fixture_file_upload('app/assets/images/flag.png')
+  end
+
+  describe '商品出品機能' do
+    context '商品出品ができるとき' do
+      it 'すべての項目が指示通りに入力されていれば出品できる' do
+        binding.pry
+      end
+    end
+
+    context '商品出品ができないとき' do
+      it '商品画像が添付されていなければ出品できない' do
+        
+      end
+      it '商品名が入力されていなければ出品できない' do
+        
+      end
+      it '商品名が４０文字以内でなければ出品できない' do
+        
+      end
+      it '商品説明が入力されていなければ出品できない' do
+        
+      end
+      it '商品説明が１０００文字以内でなければ出品できない' do
+        
+      end
+      it 'カテゴリーが選択されていなければ出品できない' do
+        
+      end
+      it '商品状態が選択されていなければ出品できない' do
+        
+      end
+      it '配送料の負担が選択されていなければ出品できない' do
+        
+      end
+      it '発送元の地域が選択されていなければ出品できない' do
+        
+      end
+      it '発送までの日数が選択されていなければ出品できない' do
+        
+      end
+      it '販売価格が入力されていなければ出品できない' do
+        
+      end
+      it '販売価格が３００〜９，９９９，９９９円以内でなければ出品できない' do
+        
+      end
+    end
+  end
 end
