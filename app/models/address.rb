ff7b8468b46adc29f1a7ff79c64d729035d923fd
Syota-Hama <77922154+Syota-Hama@users.noreply.github.com>
2022-01-17 14:09:25 +0900

    order_address.rbへバリデーションの移動

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,16 +1,3 @@
 class Address < ApplicationRecord
-
-  VALID_POST_CODE_REGEX = \A\d{3}[-]?\d{4}\z
-  VALID_TELEPHONE_NUMBER_REGEX = 0[0-9]{9,10}
-
-  with_options presence: true do
-    validates :post_code, format: { with: VALID_POST_CODE_REGEX }
-    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-    validates :municipalitis
-    validates :address
-    validates :telephone_number, format: { with: VALID_TELEPHONE_NUMBER_REGEX }
-    validates :order
-  end
-
   belongs_to :order
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,12 +1,5 @@
 class Order < ApplicationRecord
-
-  with_options presence: ture do
-    validates :user
-    validates :good
-  end
-  
   belongs_to :user
   belongs_to :good
   has_one :address
-
 end
