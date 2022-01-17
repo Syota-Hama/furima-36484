8595a90570ee8b501fe9f56b90b5a1f2469fd964
Syota-Hama <77922154+Syota-Hama@users.noreply.github.com>
2022-01-17 12:54:59 +0900

    バリデーションの設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,13 @@
 class Address < ApplicationRecord
+
+  with_options presence: true do
+    validates :post_code
+    validates :prefecture_id
+    validates :municipalitis
+    validates :address
+    validates :telephone_number
+    validates :order
+  end
+
   belongs_to :order
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,10 @@
 class Order < ApplicationRecord
 
+  with_options presence: ture do
+    validates :user
+    validates :good
+  end
+  
   belongs_to :user
   belongs_to :good
   has_one :address
