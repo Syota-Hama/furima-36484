8231d626063e8713a9aaa288ca6637b190a5da85
Syota-Hama <77922154+Syota-Hama@users.noreply.github.com>
2022-01-17 14:01:21 +0900

    郵便番号、電話番号のバリデーションの設定

--- app/models/good.rb
+++ app/models/good.rb
@@ -8,14 +8,19 @@ class Good < ApplicationRecord
   belongs_to :delivery
   has_one_attached :image
 
-  validates :items_name, presence: true, length: { in: 1..40 }
-  validates :items_explanation, presence: true, length: { in: 1..1_000 }
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :status_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :payment_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :delivery_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, numericality: { only_integer: true, message: "can't be blank" }
-  validates :image, presence: true
-  validates :price, inclusion: { in: 300..9_999_999 }
+
+  with_options presence: true do
+    validates :items_name, length: { in: 1..40 }
+    validates :items_explanation, length: { in: 1..1_000 }
+    validates :image
+  end
+
+    validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :status_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :payment_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :delivery_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :price, numericality: { only_integer: true, message: "can't be blank" }
+    
+    validates :price, inclusion: { in: 300..9_999_999 }
 end
